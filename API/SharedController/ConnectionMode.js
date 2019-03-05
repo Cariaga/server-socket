5406459b77ba581fd0241c2fd824fdaa1dbdb4b4
cariaga <cariaga.info@gmail.com>
2019-03-05 09:16:56 +0800

    Update ConnectionMode.js

--- API/SharedController/ConnectionMode.js
+++ API/SharedController/ConnectionMode.js
@@ -1,4 +1,4 @@
-
+//this is the route of the main server not its self
 let Remote = "https://tester-holdem-server.4b63.pro-ap-southeast-2.openshiftapps.com";
 let localhost = "http://localhost:8080";
 
