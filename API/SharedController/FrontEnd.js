86a9991f8391ac744c65b229edefca1646f44323
cariaga <cariaga.info@gmail.com>
2019-03-14 22:47:56 +0800

    Update FrontEnd.js

--- API/SharedController/FrontEnd.js
+++ API/SharedController/FrontEnd.js
@@ -1,4 +1,4 @@
 // do hooking for front end use based on photon
 //https://admin-holdem-front.4b63.pro-ap-southeast-2.openshiftapps.com/
-// /?OnConnection
-// /?OnPlayerAction
+// /?OnConnection - leave - join - connected - disconnected - lobby - room - leaving  
+// /?OnPlayerAction - bet - withdraw - deposit - won - lost
