fb26eda4cea3092fb08066a6caa9a71a268b9077
cariaga <cariaga.info@gmail.com>
2019-03-14 22:50:31 +0800

    Update FrontEnd.js

--- API/SharedController/FrontEnd.js
+++ API/SharedController/FrontEnd.js
@@ -1,4 +1,4 @@
-// do hooking for front end use based on photon
+// do hooking for front end use based on photon uses a more detailed apporch useful for declaring when to refresh
 //https://admin-holdem-front.4b63.pro-ap-southeast-2.openshiftapps.com/
 // /?OnConnection - leave - join - connected - disconnected - lobby - room - leaving  
-// /?OnPlayerAction - bet - withdraw - deposit - won - lost
+// /?OnPlayerAction - bet - withdraw - deposit -transfer - won - lost
